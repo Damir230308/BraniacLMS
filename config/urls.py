8986abb6f38910e52b5228b6f28cb1d55dcadb28
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-11-29 21:36:59 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,3 +23,15 @@ urlpatterns = [
     path("", views.hello_world),
     path("<str:word>/", views.check_kwargs),
 ]
+
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
+urlpatterns = [
+    path("", views.hello_world),
+    path("<str:word>/", views.check_kwargs),
+]
