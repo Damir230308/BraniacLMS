959c5384c6d65c16012b80d8673771e19af697b8
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-11-29 23:32:05 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,4 @@
 """config URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -14,8 +13,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
+]
+
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
+urlpatterns = [
+    path("", views.hello_world),
+    path("<str:word>/", views.check_kwargs),
 ]
