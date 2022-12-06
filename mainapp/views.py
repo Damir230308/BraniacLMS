b5e9862a86a4af400c23a16a3a97cf8883109172
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-12-06 21:19:13 +0500

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -35,6 +35,11 @@ class DocSitePageView(TemplateView):
 from django.views.generic import TemplateView
 
 
+from datetime import datetime
+
+from django.views.generic import TemplateView
+
+
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
@@ -47,9 +52,9 @@ class NewsPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context[
-            "news_preview"
-        ] = "Предварительное описание, которое заинтересует каждого"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
         return context
 
 
@@ -67,18 +72,3 @@ class DocSitePageView(TemplateView):
 
 class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
-class LoginPageView(TemplateView):
-    template_name = "mainapp/login.html"
-class NewsPageView(TemplateView):
-    template_name = "mainapp/news.html"
-
-    def get_context_data(self, **kwargs):
-        # Get all previous data
-        context = super().get_context_data(**kwargs)
-        # Create your own data
-        context["news_title"] = "Громкий новостной заголовок"
-        context[
-            "news_preview"
-        ] = "Предварительное описание, которое заинтересует каждого"
-        context["range"] = range(5)
-        return context
